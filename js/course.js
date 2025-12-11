d20082080372584864a0d0ab01a0db951cf35f14
rdnmarkov <rdnmarkov@gmail.com>
2025-12-11 21:14:15 +0300

    commit

--- js/course.js
+++ js/course.js
@@ -1,7 +1,7 @@
 
 const API = {
-    course: "/api/course/",
-    sendMessage: "/api/message/"
+    course: "http://109.73.207.225:8080/api/course/",
+    sendMessage: "http://109.73.207.225:8080/api/message/"
 };
 
 const params = new URLSearchParams(window.location.search);
